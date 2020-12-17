3962795a1be972ba55164b310812d3cd7ab236ca
Íèêîëàé Çàáîëîòíîâ <z.nik.vladim.312@gmail.com>
2020-12-17 12:41:13 +0300

    Фикс кодировок

--- menu.hpp
+++ menu.hpp
@@ -3,8 +3,8 @@
 
 #include "rwFunc.hpp"
 
-int menu(Building_spec* st, int arr_size); //прототип функции меню
-void print_to_scr(bool file_open, Building_spec* st, int arr_size); //прототип функции печати данных из файла на экран
-void about(); //прототип функции вывода данных о программе
+int menu(Building_spec* st, int arr_size);
+void print_to_scr(bool file_open, Building_spec* st, int arr_size);
+void about();
 
 #endif // MENU_HPP
