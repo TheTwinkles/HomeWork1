f671a19e63edaa5d4bc460f6a4404009c220cf89
Николай Заболотнов <z.nik.vladim.312@gmail.com>
2020-12-17 20:45:54 +0300

    Modifi main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main()
     system("title HomeWork1");
     system("mode con cols=89 lines=27");
     setlocale(LC_ALL,"rus");
-
-
+	
     const int arr_size = 10;
     Building_spec* building = new Building_spec[arr_size];
 
